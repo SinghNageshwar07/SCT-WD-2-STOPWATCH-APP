99a27a165c094917563cf5f7660d2779ed31e0c8
SinghNageshwar07 <nageshwar.singh97@gmail.com>
2024-08-04 15:02:12 +0530

    Add files via upload
    hello everyone i have created a STOPWATCH WEB APP by using HTML CSS and java script during an internship by skillcraft technology.

---
+++ stop watch webdev/script.js
@@ -0,0 +1,47 @@
+let startTime, updatedTime, difference, tInterval;
+let running = false;
+
+const display = document.getElementById('display');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
+
+function startTimer() {
+    if (!running) {
+        startTime = new Date().getTime() - (difference || 0);
+        tInterval = setInterval(updateTime, 10);
+        running = true;
+    }
+}
+
+function stopTimer() {
+    clearInterval(tInterval);
+    running = false;
+}
+
+function resetTimer() {
+    clearInterval(tInterval);
+    running = false;
+    difference = 0;
+    display.innerHTML = "00:00:00";
+}
+
+function updateTime() {
+    updatedTime = new Date().getTime();
+    difference = updatedTime - startTime;
+
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    display.innerHTML = 
+        (hours < 10 ? "0" + hours : hours) + ":" + 
+        (minutes < 10 ? "0" + minutes : minutes) + ":" + 
+        (seconds < 10 ? "0" + seconds : seconds);
+}
+
+// Event Listeners
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+resetButton.addEventListener('click', resetTimer);
+
